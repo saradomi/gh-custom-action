80e3882fa8c6ca2219ba99b6af1b26a598ffb656
Saraicd <saraicdomingues@gmail.com>
2025-05-28 18:50:29 +0100

    Corrected name of variable

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    // get some input values
+    // 1 get some input values
     const bucket = core.getInput('bucket', { required: true });
     const bucketRegion = core.getInput('bucket-region', { required: true });
     const distFolder = core.getInput('dist-folder', { required: true });
 
-    // upload files
+    // 2 upload files
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
